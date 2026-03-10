278d6e33f8a28d0e3f7b3769308a7d7464142396
shelo015 <shelo0_@hotmail.com>
2026-03-10 17:40:05 -0300

    Update predicciones.py

--- predicciones.py
+++ predicciones.py
@@ -3,11 +3,20 @@ import requests
 TOKEN = "8752521307:AAGT9Tq3dvkDKWOhWxbGkezM3YmnlxfeNrI"
 CHAT_ID = "7049565102"
 
-mensaje = "PRUEBA NUEVA IA 123456"
+top10 = df.sort_values("Value Local", ascending=False).head(10)
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+mensaje = "⚽ TOP APUESTAS DEL DIA\n\n"
 
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": mensaje
+for i,row in top10.iterrows():
+
+    mensaje += f"{row['Local']} vs {row['Visitante']}\n"
+    mensaje += f"Prob Local: {row['Local %']}%\n"
+    mensaje += f"Prob Visitante: {row['Visitante %']}%\n"
+    mensaje += f"Over 1.5 goles\n\n"
+
+url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(url,data={
+"chat_id":CHAT_ID,
+"text":mensaje
 })
