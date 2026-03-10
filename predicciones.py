0b54642aeac9a80e8256666fdf55675c3e82afdd
shelo015 <shelo0_@hotmail.com>
2026-03-10 17:04:44 -0300

    Update predicciones.py

--- predicciones.py
+++ predicciones.py
@@ -1,53 +1,17 @@
 import requests
-import random
 
 TOKEN = "8752521307:AAGT9Tq3dvkDKWOhWxbGkezM3YmnlxfeNrI"
 CHAT_ID = "7049565102"
 
-ligas = [4328, 4335, 4332, 4331, 4334]
+mensaje = """
+⚽ PRUEBA IA
 
-partidos = []
-
-for liga in ligas:
-
-    url = f"https://www.thesportsdb.com/api/v1/json/3/eventsnextleague.php?id={liga}"
-
-    try:
-        r = requests.get(url)
-        data = r.json()
-
-        if data and data.get("events"):
-
-            for p in data["events"]:
-                local = p["strHomeTeam"]
-                visita = p["strAwayTeam"]
-
-                partidos.append((local, visita))
-
-    except:
-        pass
-
-
-mensaje = "⚽ ANALISIS DE PARTIDOS\n\n"
-
-if len(partidos) == 0:
-    mensaje += "No se encontraron partidos en la API hoy."
-else:
-
-    partidos = partidos[:10]
-
-    for local, visita in partidos:
-
-        local_prob = random.randint(40,65)
-        visita_prob = random.randint(20,35)
-        empate_prob = 100 - (local_prob + visita_prob)
-
-        mensaje += f"{local} vs {visita}\n"
-        mensaje += f"{local} {local_prob}% de ganar\n"
-        mensaje += f"{visita} {visita_prob}% de ganar\n"
-        mensaje += f"Empate {empate_prob}%\n"
-        mensaje += "Over 1.5 goles\n\n"
-
+Liverpool vs Tottenham
+Liverpool 60% de ganar
+Tottenham 30% de ganar
+Empate 10%
+Over 1.5 goles
+"""
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
